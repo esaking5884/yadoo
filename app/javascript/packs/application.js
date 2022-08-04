52e5fc44e8a9854a424810585935c071d4c3d15c
esaking5884 <esaking5884@gmail.com>
2022-08-04 18:55:56 +0900

    install: jquery; slick to inns#show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,5 +9,20 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+//Turbolinks.start()
 ActiveStorage.start()
+require("jquery")
+
+$(function() {
+  $('.slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    pauseOnHover: false,//マウスホバーで一時停止
+    pauseOnDotsHover: false//ドットナビをマウスホバーで一時停止
+  });
+});
